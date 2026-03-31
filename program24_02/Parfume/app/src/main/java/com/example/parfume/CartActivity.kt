1f3d70f16b37c977f63953b82650744991888479
whytoriweird <romansimchich1@gmail.com>
2026-03-31 14:32:23 +0300

    wtf

--- program24_02/Parfume/app/src/main/java/com/example/parfume/CartActivity.kt
+++ program24_02/Parfume/app/src/main/java/com/example/parfume/CartActivity.kt
@@ -25,7 +25,7 @@ class CartActivity : AppCompatActivity() {
         val btnCheckout = findViewById<Button>(R.id.btnCheckout)
         val rvCart = findViewById<RecyclerView>(R.id.rvCart)
 
-        // Налаштовує адаптер і передаємо йому функцію видалення
+        // Налаштовує  і передаємо йому функцію видалення
         adapter = CartAdapter(DataStore.cart) { position ->
             DataStore.cart.removeAt(position)
             adapter.notifyItemRemoved(position)
